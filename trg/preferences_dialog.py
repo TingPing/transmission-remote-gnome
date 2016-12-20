0fb7cb66bc7c0c9d63e77e549d32cd11bd2d06af
Patrick Griffis <tingping@tingping.se>
2016-12-20 01:26:02 -0500

    Add toggle to autostart service

--- trg/preferences_dialog.py
+++ trg/preferences_dialog.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
+from os import path
 from gettext import gettext as _
 from collections import namedtuple
 from gi.repository import (
@@ -40,6 +42,8 @@ class PreferencesDialog(Gtk.Dialog):
 
 		Row = namedtuple('Row', ['title', 'widget', 'bind_property', 'setting'])
 
+		self._autostart_switch = AutoStartSwitch()
+
 		settings_map = {
 			('connection', _('Connection')): [
 				Row(_('Hostname:'), Gtk.Entry.new(), 'text', 'hostname'),
@@ -53,6 +57,7 @@ class PreferencesDialog(Gtk.Dialog):
 					'watch-downloads-directory'),
 				Row(_('Show notifications when downlods complete:'), Gtk.Switch.new(), 'active',
 				 	'notify-on-finish'),
+				Row(_('Autostart service on login:'), self._autostart_switch, '', ''),
 			]
 		}
 
@@ -63,8 +68,9 @@ class PreferencesDialog(Gtk.Dialog):
 				row_box.add(Gtk.Label.new(row.title))
 				row_box.add(row.widget)
 
-				self.settings.bind(row.setting, row.widget, row.bind_property,
-								   Gio.SettingsBindFlags.DEFAULT|Gio.SettingsBindFlags.NO_SENSITIVITY)
+				if row.bind_property and row.setting:
+					self.settings.bind(row.setting, row.widget, row.bind_property,
+									   Gio.SettingsBindFlags.DEFAULT|Gio.SettingsBindFlags.NO_SENSITIVITY)
 				box.add(row_box)
 			box.show_all()
 			self.local_stack.add_titled(box, page[0], page[1])
@@ -76,8 +82,49 @@ class PreferencesDialog(Gtk.Dialog):
 	def do_response(self, response_id):
 		if response_id == Gtk.ResponseType.APPLY:
 			self.settings.apply()
+			self._autostart_switch.apply()
 		else:
 			self.settings.revert()
 
 		if response_id != Gtk.ResponseType.DELETE_EVENT:
 			self.destroy()
+
+
+class AutoStartSwitch(Gtk.Switch):
+	def __init__(self, **kwargs):
+		super().__init__(sensitive=False, **kwargs)
+
+		autostart_file_path = path.join(GLib.get_user_config_dir(), 'autostart',
+										'se.tingping.Trg.service.desktop')
+
+		self.autostart_file = Gio.File.new_for_path(autostart_file_path)
+		logging.debug('Querying {} to see if it exists'.format(self.autostart_file))
+		self.autostart_file.query_info_async(Gio.FILE_ATTRIBUTE_STANDARD_TYPE, Gio.FileQueryInfoFlags.NONE,
+										GLib.PRIORITY_DEFAULT, callback=self._on_file_query)
+
+	def _on_file_query(self, autostart_file, result):
+		try:
+			autostart_file.query_info_finish(result)
+			self.props.active = True
+			logging.debug('Autostart file exists')
+		except GLib.Error as e:
+			self.props.active = False
+			logging.debug('Querying autostart file returned: {}'.format(e))
+		self.props.sensitive = True
+
+	def apply(self):
+		"""Creates or deletes the autostart file based upon current state"""
+		if not self.props.sensitive: # We never got the current state
+			return
+
+		if self.props.active:
+			logging.info('Creating autostart file')
+			source = Gio.File.new_for_uri('resource:///se/tingping/Trg/se.tingping.Trg.service.desktop')
+			if hasattr(source, 'copy_async'):
+				# TODO: Fix upstream in GLib
+				source.copy_async(self.autostart_file, Gio.FileCopyFlags.NONE, GLib.PRIORITY_DEFAULT)
+			else:
+				source.copy(self.autostart_file, Gio.FileCopyFlags.NONE)
+		else:
+			logging.info('Deleting autostart file')
+			self.autostart_file.delete_async(GLib.PRIORITY_DEFAULT)
