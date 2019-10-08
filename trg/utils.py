5e81117ef6aca9767199eb0fbc03e691674c241e
Patrick Griffis <tingping@tingping.se>
2019-10-08 16:53:32 -0700

    Simplify flatpak check

--- trg/utils.py
+++ trg/utils.py
@@ -7,6 +7,5 @@ _is_flatpak = None
 def is_flatpak():
     global _is_flatpak
     if _is_flatpak is None:
-        file_ = path.join(GLib.get_user_runtime_dir(), 'flatpak-info')
-        _is_flatpak = path.exists(file_)
+        _is_flatpak = path.exists('/.flatpak-info')
     return _is_flatpak
